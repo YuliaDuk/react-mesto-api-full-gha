ab594c930223400858e7131319fc88e6edf4309f
YuliaDuk <yule444ka1234@mail.ru>
2023-07-05 20:28:47 +0300

    изменен cors

--- backend/app.js
+++ backend/app.js
@@ -21,12 +21,12 @@ const auth = require('./middlewares/auth');
 const { PORT = 3000 } = process.env;
 
 const app = express();
-
+app.use(cors());
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb')
   .then(() => {
     console.log('connected to db');
   });
-app.use(cors());
+
 app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
